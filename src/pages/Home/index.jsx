9563c3704d0fc468eee71993f14b1f81dc64d54f
prajakta1003 <contact.prajaktajamdar@gmail.com>
2023-05-01 22:54:47 +0530

    Completed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
-import Header from '../../components/Home/Header'
-import SearchBar from '../../components/Home/SearchBar'
-import BlogList from '../../components/Home/BlogList'
-import { blogList } from '../../config/data'
+import React, { useState } from "react";
+import BlogList from "../../components/Home/BlogList";
+import Header from "../../components/Home/Header";
+import SearchBar from "../../components/Home/SearchBar";
+import { blogList } from "../../config/data";
+import EmptyList from "../../components/common/EmptyList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState("");
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    handleSearchResults();
+  };
+
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
+    );
+
+    setBlogs(filteredBlogs);
+  };
+
+  const handleClearSearch=()=>{
+    setBlogs(blogList);
+    setSearchKey('');
+
+  }
+
+
   return (
     <div>
-      <Header/>
-      <SearchBar/>
-      <BlogList blogs={blogList}/>
+      <Header />
+      <SearchBar
+        value={searchKey}
+        clearSearch={handleClearSearch}
+        formSubmit={handleSearchSubmit}
+        handleSearchKey={(e) => setSearchKey(e.target.value)}
+      />
 
-      
+      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
